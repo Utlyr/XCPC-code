367dde543a8d64c5bf62206a1687e3840ac530ea
Utlyr <10235501463@stu.ecnu.edu.cn>
2024-09-27 15:19:12 +0800

    2024/9/27更新

--- 数学/数论/乘法逆元.cpp
+++ 数学/数论/乘法逆元.cpp
@@ -1,2 +1,15 @@
 #include"header.h"
 //扩展欧几里得法：
+int exgcd(int a,int b,int &x,int &y)
+{
+    if(b==0)
+    {
+        x=1,y=0;
+        return a;
+    }
+    int d=exgcd(b,a%b,x,y);
+    int k=x;
+    x=y;
+    y=k-a/b*y;
+    return d;
+}
