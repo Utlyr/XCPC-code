b848ef6595138750d5a3aa050d5ec98a375c590f
Utlyr <10235501463@stu.ecnu.edu.cn>
2024-10-15 21:01:57 +0800

    2024/10/15更新

--- 数学/组合数学/十二重计数法.cpp
+++ 数学/组合数学/十二重计数法.cpp
@@ -1,6 +1,6 @@
 #include"header.h"
-const int MAXN = 4e5+10;
-const int mod = 998244353,g=3,ig=332748118;
+#include"C:\Users\34065\Desktop\mygit\XCPC-code\数学\多项式与生成函数\多项式模板.cpp"
+
 int ksm(int a,int b,int p)
 {
     int ans=1;
@@ -47,44 +47,14 @@ int count3(int n,int m)
     int ans=0,f=1;
     for(int i=0;i<=m;i++)
     {
-        ans=(ans+f*c(m,i)*ksm(i,n,mod)+mod)%mod;
-        f*=-1;
+        
+        if((m-i)%2==1)ans=(ans+(mod-c(m,i)*ksm(i,n,mod)%mod)%mod)%mod;
+        else ans=(ans+(c(m,i)*ksm(i,n,mod)%mod)%mod+mod)%mod;
     }
     return ans;
 }
 
-int rev[2100000];
 
-int getl(int len)
-{
-    int n=1;
-    while(n<=len)n<<=1;
-    for(int i=0;i<n;i++)rev[i]=(rev[i>>1]>>1)|((i&1)?(n>>1):0);
-    return n;
-}
-void NTT(vector<int> &x,int n,int mode)//每次NTT前调用getl进行初始化rev,n为2的幂次
-{
-    for(int i=0;i<n;i++)if(i<rev[i])swap(x[i],x[rev[i]]);
-    for(int len=1;len<n;len<<=1)
-    {
-        int Wn=ksm(mode?g:ig,(mod-1)/(2*len),mod);
-        for(int i=0;i<n;i+=2*len)
-        {
-            int W=1,X,Y;
-            for(int j=i;j<i+len;j++)
-            {
-                X=x[j];Y=W*x[j+len]%mod;
-                x[j]=(X+Y)%mod;x[j+len]=(X-Y+mod)%mod;
-                W=(W*Wn)%mod;
-            }
-        }
-    }
-    if(!mode)
-    {
-        int invs=ksm(n,mod-2,mod);
-        for(int i=0;i<n;i++)x[i]=(x[i]*invs)%mod;
-    }
-}
 //4.球之间互不相同，盒子全部相同。（第二类斯特林数行的和）
 int count4(int n,int m)
 {
@@ -101,7 +71,7 @@ int count4(int n,int m)
     for(int i=0;i<limit;i++)f1[i]=f1[i]*g1[i]%mod;
     NTT(f1,limit,0);
     int ans=0;
-    for(int i=0;i<=m;i++)ans=(ans+f1[i])%mod;
+    for(int i=0;i<=min(m,n);i++)ans=(ans+f1[i])%mod;
     return ans;
 }
 
@@ -119,8 +89,8 @@ int count6(int n,int m)
     int ans=0,f=1;
     for(int i=0;i<=m;i++)
     {
-        ans=(ans+f*inv[m-i]*inv[i]%mod*ksm(i,n,mod)%mod+mod)%mod;
-        f*=-1;
+        if((m-i)%2==1)ans=(ans+(mod-inv[m-i]*inv[i]%mod)*ksm(i,n,mod)%mod+mod)%mod;
+        else ans=(ans+inv[m-i]*inv[i]%mod*ksm(i,n,mod)%mod+mod)%mod;
     }
     return ans;
 }
@@ -144,11 +114,22 @@ int count9(int n,int m)
     if(m>n)return 0;
     return c(n-1,m-1);
 }
-
+vector<int> f1(MAXN),g1(MAXN);
 //10.球全部相同，盒子全部相同
 int count10(int n,int m)
 {
-    return 0;
+    int limit=getl(n+1);
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=i;j<=n;j+=i)
+        {
+            f1[j]+=ksm(j/i,mod-2);
+        }
+    }
+    polyexp(f1,g1,limit);
+    int t=g1[n];
+    for(int i=0;i<limit;i++)f1[i]=g1[i]=0;
+    return t;
 }
 
 //11.球全部相同，盒子全部相同，每个盒子至多装一个球。
